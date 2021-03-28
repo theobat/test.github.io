21b18dd76ef14757dbd4c60902e341a1e5476d70
viktorsmari <viktorsmari@users.noreply.github.com>
2021-03-28 18:54:14 +0000

    deploy: ffd11da57ead4444ca2d8c03eb81b7ced5aa24a8

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5d8f45a9024302cae19bb013bfbfc87c.js"
+  "/precache-manifest.88a8cf4df573acb1036f6dec6ba40270.js"
 );
 
 self.addEventListener('message', (event) => {
