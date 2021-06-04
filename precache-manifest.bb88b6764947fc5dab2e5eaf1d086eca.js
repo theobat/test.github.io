53e4b3537eb783307de024b81c11334376494973
viktorsmari <viktorsmari@users.noreply.github.com>
2021-06-04 07:44:54 +0000

    deploy: bc6b6b5cd446576cbd2148d4b272455906347a07

---
+++ precache-manifest.bb88b6764947fc5dab2e5eaf1d086eca.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc23c8470d3bf8a15b9498a50afa2932",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a17c17b0f513e2c5fc1",
+    "url": "/static/css/3.d58abfc1.chunk.css"
+  },
+  {
+    "revision": "fceced95ee636a23c03c",
+    "url": "/static/css/main.715072af.chunk.css"
+  },
+  {
+    "revision": "8a17c17b0f513e2c5fc1",
+    "url": "/static/js/3.c289da53.chunk.js"
+  },
+  {
+    "revision": "a330924ede23895097e938f18a50381b",
+    "url": "/static/js/3.c289da53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89ea886a751ab33484c0",
+    "url": "/static/js/i18n-0.cbd555d2.chunk.js"
+  },
+  {
+    "revision": "fceced95ee636a23c03c",
+    "url": "/static/js/main.53fa9185.chunk.js"
+  },
+  {
+    "revision": "3888081023e42e037b3f",
+    "url": "/static/js/runtime-main.c6e8918f.js"
+  },
+  {
+    "revision": "331e6b36b956cea4190238d9efae9224",
+    "url": "/static/media/by-sa.331e6b36.png"
+  },
+  {
+    "revision": "04d377d968557a6092bccb1617db5a65",
+    "url": "/static/media/by.04d377d9.png"
+  },
+  {
+    "revision": "43589a77a15d62dc16d5c40615634a21",
+    "url": "/static/media/collectionPlaceholder.43589a77.png"
+  },
+  {
+    "revision": "61b593fefe98b7f2ad9871336efaf176",
+    "url": "/static/media/messages.61b593fe.po"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dfa0e666b04bcbe4c5416af99693d62c.js"
+  "/precache-manifest.bb88b6764947fc5dab2e5eaf1d086eca.js"
 );
 
 self.addEventListener('message', (event) => {
