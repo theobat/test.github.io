e4e6b86b8c55ea027b1e10395b408b6346ba013b
pral2a <pral2a@users.noreply.github.com>
2022-03-04 12:29:17 +0000

    deploy: b7a6931783d6dcfcb698f349a850376942855391

---
+++ precache-manifest.1b57d78410e52f51efdc45584e724f4d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3100c16a09999402f11f64facd4b1690",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4971a486877dbfa44daf",
+    "url": "/static/css/3.02432556.chunk.css"
+  },
+  {
+    "revision": "bbd02a30b18e554d1b5d",
+    "url": "/static/css/main.715072af.chunk.css"
+  },
+  {
+    "revision": "4971a486877dbfa44daf",
+    "url": "/static/js/3.ee39c5af.chunk.js"
+  },
+  {
+    "revision": "026d7d0017202ac2555ebf4979d71b76",
+    "url": "/static/js/3.ee39c5af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a70d32d4efa052ab49d",
+    "url": "/static/js/i18n-0.c2cfa868.chunk.js"
+  },
+  {
+    "revision": "bbd02a30b18e554d1b5d",
+    "url": "/static/js/main.e4469e8e.chunk.js"
+  },
+  {
+    "revision": "eb5cb032fc02239c0298",
+    "url": "/static/js/runtime-main.dbbbd252.js"
+  },
+  {
+    "revision": "94f7d9e454813194f8f18e0a14f0666e",
+    "url": "/static/media/messages.94f7d9e4.po"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.da6ed154fffeda96a03d0722c4050cfc.js"
+  "/precache-manifest.1b57d78410e52f51efdc45584e724f4d.js"
 );
 
 self.addEventListener('message', (event) => {
